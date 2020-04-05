bf5be08fdbf3ba4dfd2cdc6a75f188dbbadba0d8
ammar <user@Users-MacBook-Pro.local>
2020-04-05 12:17:51 +0100

    cleanup - typescript coding standard

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -14,7 +14,7 @@ import fs from 'fs';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get( "/filteredimage", async ( req, res) => {
+  app.get( "/filteredimage", async (req:express.Request, res:express.Response) => {
 
     let url = req.query.url;
 
@@ -45,7 +45,7 @@ import fs from 'fs';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async (req:express.Request, res:express.Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
