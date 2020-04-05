f76f7d899a7d5f56166efd66358f2338484310eb
ammar <user@Users-MacBook-Pro.local>
2020-04-05 01:00:55 +0100

    GET request - send processed file and delete temp file once finshed

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import Jimp = require('jimp');
+import { reject } from 'bluebird';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,18 +10,32 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+
+    return new Promise( async (resolve, reject) => 
+    
+    {
+        try
+        {
+            const photo = await Jimp.read(inputURL);
+            const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+            await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname+outpath, (img)=>{
+                resolve(__dirname+outpath);
+            });
+        }catch(err){
+            console.log('err msg: ' + err.message);
+            reject('Not publicly accessible image');
+        }
     });
 }
+
+  process.on('unhandledRejection', error => {
+  // Will print "unhandledRejection err is not defined"
+  console.log('unhandledRejection', error);
+});
 
 // deleteLocalFiles
 // helper function to delete files on the local disk
@@ -29,6 +44,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files:Array<string>){
     for( let file of files) {
+        console.log('file: ' + file)
         fs.unlinkSync(file);
     }
 }
